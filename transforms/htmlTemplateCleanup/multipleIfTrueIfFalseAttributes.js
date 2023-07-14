62a22564f2fbfbe17b9d47c061ef5232fc6ba6b9
James Tu <j.tu@salesforce.com>
2023-07-14 12:12:52 -0700

    chore: fix formatting

--- transforms/htmlTemplateCleanup/multipleIfTrueIfFalseAttributes.js
+++ transforms/htmlTemplateCleanup/multipleIfTrueIfFalseAttributes.js
@@ -9,25 +9,25 @@ import { walkParse5Ast, serializeParse5Ast } from '../parse5Utils'
 const isConditionalAttr = (attr) => attr.name === 'if:true' || attr.name === 'if:false'
 
 export const fixMultipleIfTrueIfFalseAttributes = ({ ast, file }, result) => {
-    let modifiedSource = false
+  let modifiedSource = false
 
-    walkParse5Ast(ast, (node) => {
-        // Remove duplicate if:true/if:false attributes
-        const conditionalAttrs = node.attrs?.filter(isConditionalAttr)
-        if (conditionalAttrs?.length > 1) {
-            // In LWC only the first if:true/if:false is processed
-            // Keep the first occurrence and discard the rest
-            node.attrs = node.attrs.filter(
-                (attr) => !isConditionalAttr(attr) || attr === conditionalAttrs[0]
-            )
+  walkParse5Ast(ast, (node) => {
+    // Remove duplicate if:true/if:false attributes
+    const conditionalAttrs = node.attrs?.filter(isConditionalAttr)
+    if (conditionalAttrs?.length > 1) {
+      // In LWC only the first if:true/if:false is processed
+      // Keep the first occurrence and discard the rest
+      node.attrs = node.attrs.filter(
+        (attr) => !isConditionalAttr(attr) || attr === conditionalAttrs[0]
+      )
 
-            modifiedSource = true
-        }
-    })
-
-    if (modifiedSource) {
-        result.overwrite[file] = serializeParse5Ast(ast)
+      modifiedSource = true
     }
+  })
 
-    return result
+  if (modifiedSource) {
+    result.overwrite[file] = serializeParse5Ast(ast)
+  }
+
+  return result
 }
